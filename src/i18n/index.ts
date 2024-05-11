14d1d75eff99862af138a5d1f1c48ec44629519e
AkiChase <1003019131@qq.com>
2024-05-11 16:53:37 +0800

    fix(i18n): build error

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,15 +5,17 @@ import enUS from "./en-US.json";
 import zhCN from "./zh-CN.json";
 
 const localStore = new Store("store.bin");
-const language = (await localStore.get<string>("language")) ?? "en-US";
 
 const i18n = createI18n({
-  locale: language,
   allowComposition: true,
   messages: {
     "en-US": enUS,
     "zh-CN": zhCN,
   },
+});
+
+localStore.get<"en-US" | "zh-CN">("language").then((language) => {
+  i18n.global.locale = language ?? "en-US";
 });
 
 export default i18n;
